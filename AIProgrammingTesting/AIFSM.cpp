0e368259914f4a40df34ae8981251c79e574459c
Andreas Jonasson <andreas.jonasson1993@gmail.com>
2017-07-12 12:00:20 +0200

    Added a new class

---
+++ AIProgrammingTesting/AIFSM.cpp
@@ -0,0 +1,13 @@
+﻿#include "AIFSM.h"
+
+AIFSM::AIFSM(uint16_t life, float positionX, float positionY) : AIBase(life, positionX, positionY)
+{
+}
+
+AllowedActions AIFSM::DecideAction(std::vector<AIBase*> allAIs, unsigned myID, Map2D* map)
+{
+}
+
+AIFSM::~AIFSM()
+{
+}
---
+++ AIProgrammingTesting/AIFSM.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "AIBase.h"
+
+class AIFSM : public AIBase/*Side 48 in Game AI Pro*/
+{
+public:
+	AIFSM(uint16_t life, float positionX, float positionY);
+
+	AllowedActions DecideAction(std::vector<AIBase*> allAIs, unsigned myID, Map2D* map) override;
+	~AIFSM() override;
+
+private:
+	std::vector<int> temp;
+};
